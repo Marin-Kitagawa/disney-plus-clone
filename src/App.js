fd9a2515f64bbae328edb8514799951bf72dffe0
The Rubyist <49131888+ruby-rust-perl@users.noreply.github.com>
2022-02-22 10:04:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
-
+import Login from './components/Login';
+import Header from './components/Header';
+import Home from './components/Home';
+import Detail from './components/Detail';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+         <Router>
+           <Header/>
+           <Switch>
+             <Route exact path='/'>
+               <Login />
+             </Route>
+             <Route path="/home">
+               <Home />
+             </Route>
+           </Switch>
+           <Route path='/detail/:id'>
+             <Detail />
+           </Route>
+         </Router> 
     </div>
   );
 }
